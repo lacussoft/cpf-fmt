47f3b99b9ac71f938922058b4afd3fc0c9f80544
Julio Muller <juliodolago@gmail.com>
2021-01-15 09:13:58 -0300

    Fixed typing file

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,5 +14,6 @@ export type CpfFormattingOptions = {
   escape?: boolean;
 };
 
-export const cpfFmt: (cpfString: string, options?: CpfFormattingOptions) => string;
+declare const cpfFmt: (cpfString: string, options?: CpfFormattingOptions) => string;
+
 export default cpfFmt;
